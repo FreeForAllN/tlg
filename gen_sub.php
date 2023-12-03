1450273f88e55c538f5321fdcf51c34c5fe66c69
FreeForAllN <152710305+FreeForAllN@users.noreply.github.com>
2023-12-03 12:52:59 +0330

    update

--- gen_sub.php
+++ gen_sub.php
@@ -83,9 +83,15 @@ $output = "VMESS LINKS:\n" . implode("\n", $vmessLinks) . "\n\n" .
           "VLESS LINKS:\n" . implode("\n", $vlessLinks) . "\n\n" .
           "TROJAN LINKS:\n" . implode("\n", $trojanLinks) . "\n\n" .
           "SHADOWSOCKS LINKS:\n" . implode("\n", $ssLinks);
-
+$vm64=base64_encode($vmessLinks)
+$vl64=base64_encode($vlessLinks)
+$tj64=base64_encode($trojanLinks)
+$ss64=base64_encode($ssLinks)
 // Write the combined links to a .txt file
-
+$output64 = "VMESS LINKS:\n" . implode("\n", $vm64) . "\n\n" .
+          "VLESS LINKS:\n" . implode("\n", $vl64) . "\n\n" .
+          "TROJAN LINKS:\n" . implode("\n", $tj64) . "\n\n" .
+          "SHADOWSOCKS LINKS:\n" . implode("\n", $ss64);
 
 
 
@@ -95,7 +101,7 @@ if (!is_dir('sub')) {
     mkdir('sub', 0755, true);
 }
 file_put_contents('sub/output', $output);
-file_put_contents('sub/s64', base64_encode($output));
+file_put_contents('sub/s64', $output64);
 
 ?>
 
